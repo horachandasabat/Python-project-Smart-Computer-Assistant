1cae1b517742973db7bf0801a77f3d9b3628b43d
horachandasabat <lsabat352@gmail.com>
2024-08-12 23:39:20 +0530

    Add files via upload

---
+++ jarviscomplete.py
@@ -0,0 +1,55 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import QTimer, QTime, QDate, Qt
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+from jarvisUi import Ui_MainWindow
+import jarvis
+import sys
+
+
+class MainThread(QThread):
+
+    def __init__(self):
+        super(MainThread,self).__init__()
+
+    def run(self):
+        jarvis.TaskExecution()
+
+startExecution = MainThread()
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.startTask)
+        self.ui.pushButton_2.clicked.connect(self.close)
+
+    def startTask(self):
+        self.ui.movie = QtGui.QMovie("D:\Desktop\jarvisGui\image_processing20210907-13511-1juj33d.gif")
+        self.ui.label.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(1000)
+        startExecution.start()
+
+    def showTime(self):
+        current_time = QTime.currentTime()
+        current_date = QDate.currentDate()
+        label_time = current_time.toString('hh:mm:ss')
+        label_date = current_date.toString(Qt.ISODate)
+        self.ui.textBrowser.setText(label_time)
+        self.ui.textBrowser_2.setText(label_date)
+
+        startExecution.start()
+
+GuiApp = QApplication(sys.argv)
+jarvis_gui = Main()
+jarvis_gui.show()
+exit(GuiApp.exec_())
+
